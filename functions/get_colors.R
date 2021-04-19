4e8c98af3c3699fde6829cdc20edc1bde7759e26
Simon Metzger <Simon.Metzger98@gmail.com>
2021-04-19 12:59:57 +0200

    Troubleshooting get_colors function

--- functions/get_colors.R
+++ functions/get_colors.R
@@ -1,7 +1,7 @@
 get_colors <- function(is_custom = FALSE, color_scheme = NULL, custom_colors = NULL){
   if(!is_custom){
     if(color_scheme %in% rownames(brewer.pal.info)){
-      color <- brewer.pal(n = 8, input$color_scheme)
+      color <- brewer.pal(n = 8, color_scheme)
     } else {
       color <- get_color_scheme(color_scheme)
     }
